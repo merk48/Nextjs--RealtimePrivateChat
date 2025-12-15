aa3c83fe887e55ecef688117e52d4676f41f3a47
Merk48 <meryKassis48@gmail.com>
2025-12-15 04:02:59 +0300

    feat: getting message history for both useres

--- src/hooks/use-usename.ts
+++ src/hooks/use-usename.ts
@@ -1,15 +1,16 @@
 import { nanoid } from "nanoid";
 import { useEffect, useState } from "react";
 
-const ANIMALS = ["wolf", "lion", "bear", "hawk"];
+const ANIMALS = ["wolf", "hawk", "bear", "shark"];
 const STORAGE_KEY = "chat_username";
+
 const generateUsername = () => {
   const word = ANIMALS[Math.floor(Math.random() * ANIMALS.length)];
-
   return `anonymous-${word}-${nanoid(5)}`;
 };
+
 export const useUsername = () => {
-  const [username, setUsername] = useState<string>("");
+  const [username, setUsername] = useState("");
 
   useEffect(() => {
     const main = () => {
